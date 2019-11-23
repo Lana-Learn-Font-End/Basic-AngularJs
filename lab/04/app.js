09e6608dd5374085888615f70daa333264a96107
lana <sitdownrightnow2552@gmail.com>
2019-11-24 01:01:43 +0700

    lab04 display item

--- lab/04/app.js
+++ lab/04/app.js
@@ -1,8 +1,34 @@
-const app = angular.module("productApp", [
-    'ngRoute',
-    ''
-]);
+const app = angular.module("productApp", ['ngRoute']);
+app.controller("ProductCtrl", function ProductCtrl($http) {
+    const vm = this;
+    vm.data = [];
+    $http
+        .get("product.json")
+        .then(products => vm.data = products.data)
+        .catch(err => console.log(err))
+});
 
-app.config("$routeProvider", function () {
+app.component("appTable", {
+    templateUrl: "templates/table.html",
+    bindings: {data: "="}
+});
 
+app.component("appCardList", {
+    templateUrl: "templates/card-list.html",
+    bindings: {data: "="}
+});
+
+app.component("appNav", {
+    templateUrl: "templates/nav.html",
+    controller: function NavBarCtrl($location) {
+        const vm = this;
+        vm.loc = $location;
+    }
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/table", {template: "<app-table data='ctrl.data'></app-table>"})
+        .when("/card", {template: "<app-card-list data='ctrl.data'></app-card-list>"})
+        .otherwise({redirectTo: "/table"})
 });
