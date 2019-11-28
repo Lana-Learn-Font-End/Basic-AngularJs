c64015e0140b8b4161e89cc1f5caf2d6245f26b9
lana <sitdownrightnow2552@gmail.com>
2019-11-28 16:37:34 +0700

    display product list

--- lab/09/app.js
+++ lab/09/app.js
@@ -1,1 +1,22 @@
-const app = angular.module("loginProduct", []);
+const app = angular.module("loginProduct", ["ngRoute"]);
+
+app.controller("AppCtrl", function ($http) {
+    const ctrl = this;
+    ctrl.data = [];
+    $http
+        .get("../product.json")
+        .then(res => ctrl.data = res.data.filter(item => item != undefined));
+
+    ctrl.delete = index => ctrl.data = ctrl.data.filter((_, i) => i !== index);
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "table.html",
+        controller: "AppCtrl",
+        controllerAs: "$ctrl"
+    });
+    $routeProvider.when("/login", {
+        template: "<app-login></app-login>"
+    })
+});
