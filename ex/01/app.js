c948a76b03c3abf0dbfa1b24d8f1d8f980dfcb94
Lana <sitdownrightnow2552@gmail.com>
2019-11-30 00:33:54 +0700

    add login handle

--- ex/01/app.js
+++ ex/01/app.js
@@ -3,8 +3,27 @@ const app = angular.module("app", ["ngRoute"]);
 app.controller("MainCtrl", function ($scope, $http) {
     $scope.data = [];
     $scope.loggedInUser = undefined;
-    $scope.username = "";
-    $scope.password = "";
+    $scope.loginFailed = false;
+    $scope.loginForm = {};
+
+    $scope.login = function () {
+        $scope.loggedInUser = $scope.data.find((item) =>
+            item.username === $scope.loginForm.username
+            && item.password === $scope.loginForm.password
+        );
+        if (!$scope.loggedInUser) $scope.loginFailed = true;
+    };
+
+    $scope.logout = function () {
+        $scope.loggedInUser = undefined;
+        $scope.reset();
+    };
+
+    $scope.reset = function () {
+        angular.copy({}, $scope.loginForm);
+        $scope.loginFailed = false;
+    };
+
     $http
         .get("data/students.json")
         .then((res) => $scope.data = res.data)
@@ -22,9 +41,11 @@ app.component("appHome", {
              Remove password field from the student data.
              This prevent the miss behaviour of filter when do fuzzy search which make the
              result contains student that have password contains the search key.
-
              For example, delete this $onChanges and try to search 1 to see the weird results
              (results is all student, because their password contains 1).
+
+             Alternatively, if you want that behaviour, you can just delete this ctrl.$onChanges function
+             and everything is going to work as it is before, and the password gonna display in the modal
 
              Note that this only affect the data in this controller, as i used one-way binding on the
              data array.
