92012435d2d47d8f5e1734beefe46336da2fff49
Lana <sitdownrightnow2552@gmail.com>
2019-11-30 21:00:48 +0700

    ex02

--- ex/02/app.js
+++ ex/02/app.js
@@ -1,5 +1,56 @@
 const app = angular.module("app", ["ngRoute"]);
 
-app.controller("MainCtrl", function () {
-
+app.component("appModal", {
+    templateUrl: "page/modal.html",
+    controller: function ModalController($element) {
+        const ctrl = this;
+        ctrl.$onChanges = function () {
+            ctrl.show
+            ? $element.children(".modal").modal("show")
+            : $element.children(".modal").modal("hide");
+        };
+    },
+    transclude: true,
+    bindings: {
+        show: "<",
+    }
 });
+
+app.controller("MainCtrl", function ($scope, $http) {
+    $scope.data = [];
+    $scope.studentDetail = undefined;
+    $scope.studentMark = undefined;
+    $scope.studentMarkAvg = 0;
+    $http
+        .get("data/students.json")
+        .then((res) => $scope.data = res.data);
+
+    $scope.closeDetail = function () {
+        $scope.studentDetail = undefined;
+    };
+
+    $scope.showDetail = function (index) {
+        $scope.studentDetail = $scope.data[index];
+    };
+
+    $scope.checkMark = function (code) {
+        $scope.studentMark = $scope.data.find((student) => student.code === code);
+        if ($scope.studentMark)
+            $scope.studentMarkAvg = ($scope.studentMark.mark_html +
+                                     $scope.studentMark.mark_ajs +
+                                     $scope.studentMark.mark_dwhcj) / 3;
+        else
+            alert("Sinh viên không tồn tại!");
+    };
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        templateUrl: "page/home.html"
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: "/home",
+    });
+});
+
