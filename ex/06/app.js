09831303675752d6732636f1f428604c30815208
Lana <sitdownrightnow2552@gmail.com>
2019-12-01 23:14:44 +0700

    ex06

--- ex/06/app.js
+++ ex/06/app.js
@@ -1,5 +1,29 @@
 const app = angular.module("app", ["ngRoute"]);
 
-app.controller("MainCtrl", function () {
+app.controller("MainCtrl", function ($scope, $http, $element) {
+    $scope.data = [];
+    $scope.cartData = [];
+    $http.get("data/products.json").then((res) => $scope.data = res.data);
 
+    $scope.getTotal = function () {
+        return $scope.cartData.reduce((total, current) => total + current.price, 0);
+    };
+
+    $scope.addToCart = function (item) {
+        $scope.cartData.push(item);
+    };
+
+    $scope.showDetail = function (item) {
+        $scope.productDetail = item;
+        $element.children("#modal-show").modal("show");
+    };
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        templateUrl: "page/home.html"
+    });
+    $routeProvider.otherwise({
+        redirectTo: "/home",
+    });
 });
